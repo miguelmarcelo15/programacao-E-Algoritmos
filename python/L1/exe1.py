1fbf415a8bcf1d40adebac7af20e4de80fa6c1b4
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-06 15:09:15 -0300

    Create exe1.py

---
+++ python/L1/exe1.py
@@ -0,0 +1,9 @@
+
+numero = int(input("digite um número inteiro: "))
+
+if numero > 0:
+    print("o numero é positovo! ")
+elif numero < 0:
+    print("o numero é negativo! ")
+else:
+    print("o número é 0! ")
