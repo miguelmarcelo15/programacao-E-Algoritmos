a3db4faf58d8f7ed2148d23e728450c88e283cdd
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-06 15:26:12 -0300

    Create exe2.py

---
+++ python/L1/exe2.py
@@ -0,0 +1,9 @@
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+
+if numero1 > numero2: 
+    print("O maior numero é: ", numero1 ,"\n") 
+elif numero2 > numero1:
+   print("O maior numero é: ", numero2 ,"\n")   
+else:
+    print("Os numeros são iguais")
