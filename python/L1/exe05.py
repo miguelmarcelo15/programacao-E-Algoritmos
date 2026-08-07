6f2ee0162613f7623ec469316656daf4d973f48b
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-07 20:21:06 -0300

    Update exe05.py

--- python/L1/exe05.py
+++ python/L1/exe05.py
@@ -1,1 +1,9 @@
+numero = int(input("fale um número de 1 a 10? "))
+i = 1
+
+while numero < 1 or numero > 10:
+  numero = int (input ("seu número é invalido. tente novamente: "))
+while i <= 10:
+    print(numero, "x", i, "=", numero * i, "\n")
+    i += 1
 
