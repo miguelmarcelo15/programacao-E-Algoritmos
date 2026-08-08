f99e643f1a5baa65fbc0297933534b5559a2d74c
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-07 22:03:42 -0300

    Update exe09.py

--- python/L1/exe09.py
+++ python/L1/exe09.py
@@ -1,1 +1,11 @@
+numero = int (input ("escreva um numero inteiro positivo: "))
+i = 1
+fatorial = 1
+
+
+while (numero > 0 and i <= numero):
+    fatorial = fatorial * i
+    i += 1
+print("o número ",numero, "em fatorial é : ",fatorial)
+
 
