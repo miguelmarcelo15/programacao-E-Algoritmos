3e13da9bbbb720562f27f8207f9d92be48ba9073
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-07 21:19:46 -0300

    Update exe07.py

--- python/L1/exe07.py
+++ python/L1/exe07.py
@@ -1,1 +1,20 @@
+positivo = 0
+negativo = 0
+zero = 0
+i = 1
 
+while i <= 10:
+    numero = int(input(f"Digite o {i}º número: "))
+
+    if numero > 0:
+        positivo += 1
+    elif numero < 0:
+        negativo += 1
+    else:
+        zero += 1
+
+    i += 1
+
+print("Positivos:", positivo)
+print("Negativos:", negativo)
+print("Zeros:", zero)
