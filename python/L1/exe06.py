2330e1695718872cd3d066bc5ec004b161f546d9
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-10 16:07:23 -0300

    Update exe06.py

--- python/L1/exe06.py
+++ python/L1/exe06.py
@@ -1,1 +1,13 @@
+nota = float(input("Digite uma nota entre 0 e 10: "))
+quantidade = 0
+soma = 0
 
+while nota > 0 and nota <= 10:
+    soma = soma + nota
+    quantidade += 1
+    nota = float(input("Digite outra nota ou um número fora de 0-10 para sair: "))
+    
+if quantidade > 0: 
+    print("Média das notas: ", soma / quantidade)
+else:
+    print("Nenhuma nota válida foi digitada.")
