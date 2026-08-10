d659fb1693bbf7cc3d0e50a690bbbd24940a0f47
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-10 14:50:24 -0300

    Update exe04.py

--- python/L1/exe04.py
+++ python/L1/exe04.py
@@ -1,1 +1,15 @@
+numero = int(input ("Digite um numero inteiro positivo: "))
+i = 1
+soma = 0
+
+
+if numero > 0:
+    while i <= numero:
+        if i % 2 == 0:
+            soma = soma + i 
+        i += 1
+    print ("Soma dos pares é: ", soma )          
+
+else:
+    print("Numero invalido. Digite um valor positivo.")
 
