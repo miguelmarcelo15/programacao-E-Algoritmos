c96f964f59f7cc7827c55ab70e8fd4853b300122
miguelmarcelo15 <miguel.m.c.o.santos@gmail.com>
2026-08-10 16:37:56 -0300

    Update exe08.py

--- python/L1/exe08.py
+++ python/L1/exe08.py
@@ -1,1 +1,17 @@
+numero = int(input("Digite uma numero inteiro positivo: "))
+divisor= 1
+quantidadeDivisores = 0
+
+if numero > 0:
+    while divisor <= numero:
+        if numero % divisor == 0:
+            quantidadeDivisores += 1
+        divisor += 1
+    if quantidadeDivisores == 2:
+      print("O número é primo.")
+    else:
+      print("O número não é primo.")
+else:
+    print("O número não é primo.(invalido)")
+
 
